b8287edebf6e1c5464923e9109a7cb83ddf7d5c7
Emadaddin <Emad@MacBook-Air.local>
2025-08-01 15:01:27 +0300

    added delete account button and confirmation. Also created vertical tabs

--- manager/pages/account_page.py
+++ manager/pages/account_page.py
@@ -1,4 +1,4 @@
-from nicegui import ui, App
+from nicegui import ui, app
 from models import User, Transaction
 
 def account_page():
@@ -7,4 +7,45 @@ def account_page():
             ui.label('MoneyManager').classes('bg-gradient-to-r from-cyan-400 to-blue-700')\
                     .classes(f'bg-clip-text text-transparent text-4xl')
             ui.button('Dashboard', on_click=ui.navigate.back).props('rounded outline').classes('w-full')
+
+            with ui.dialog() as dialog, ui.card().classes('items-center'):
+                ui.label('Are you sure you want to delete your account?').classes('font-semibold text-xl')
+                ui.label('*This can\'t be reversed').classes('text-sm text-grey')
+                with ui.row().classes('justify-between'):
+                    ui.button('Yes', on_click=lambda: dialog.submit('Yes')).classes('bg-red text-white')
+                    ui.button('No', on_click=lambda: dialog.submit('No'))
+            
+            async def show_dialog():
+                result = await dialog
+                if result == 'Yes':
+                    user = User(app.storage.user.get('username'), app.storage.user.get('name'))
+                    user.delete_account()
+                    ui.navigate.to('/')
+                    return
+                else:
+                    return
+            ui.button('Delete Account', on_click=show_dialog).classes('bg-red w-full text-white').props('rounded outline')
+    
+    def account_info():
+        with ui.card().classes('w-full h-full'):
+            ui.markdown(f"**Username:** {app.storage.user.get('username')}").classes('text-6xl')
+            ui.markdown(f"**Name:** {app.storage.user.get('name')}").classes('text-4xl')
+
+    def graph_info():
+        ui.label('Graph here')
+
+    def main():
+        with ui.row().classes('w-full h-full'):
+            with ui.tabs().props('vertical') as tabs:
+                account = ui.tab('Account Info')
+                graph = ui.tab('Statistics')
+            with ui.tab_panels(tabs, value=account).props('vertical'):
+                with ui.tab_panel(account):
+                    account_info()
+                with ui.tab_panel(graph):
+                    graph_info()
+
+    main()
     left_drawer()
+
+
